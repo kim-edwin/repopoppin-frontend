08f54912a5fb10ff19fd03d2df811c56685fb5a5
Hyeonu Kim <woo813625@gmail.com>
2024-03-05 15:56:45 +0900

    build file update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./router";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { BrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "./theme";
 
@@ -14,9 +14,7 @@ root.render(
     <QueryClientProvider client={client}>
         <ChakraProvider theme={theme}>
             <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <RouterProvider router={router} />
-            </BrowserRouter>
         </ChakraProvider>
     </QueryClientProvider>,
 );
